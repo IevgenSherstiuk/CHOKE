1e5a8d3f61a1da8f2c73641cd46d8c588a45e93a
Ievgen Sherstiuk <73878060+IevgenSherstiuk@users.noreply.github.com>
2023-12-28 17:44:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,20 +7,60 @@ from btns_handler import btns_from_df, update_btns
 async def main(page: ft.Page):
 
  
-    page.title = "Калькулятор цены дроселя"
-    page.scroll = True
-    page.vertical_alignment = ft.MainAxisAlignment.START
-    page.horizontal_alignment = 'center'
+    page.title = "Розрахунок дроселя"
+    # page.scroll = True
+    # page.vertical_alignment = ft.MainAxisAlignment.START
+    # page.horizontal_alignment = 'center'
     page.theme_mode = 'light'
     page.theme = ft.Theme(color_scheme_seed=ft.colors.LIGHT_BLUE_700)
 
     #Inputs
-    I = ft.TextField(label="Current Intensity, A", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
-    L = ft.TextField(label="Inductance, mGn", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    I = ft.TextField(label="Струм, А", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
     I.value = 1
+    L = ft.TextField(label="Індуктивність, мГн", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
     L.value = 50
 
     #Additional Inputs
+    Bmax = ft.TextField(label="Робоча індукція(B), Тл", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    Bmax.value = 0.4
+    Kf = ft.TextField(label="Коефіцієнт феромагнітного заповнення", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    Kf.value = 0.7
+    q = ft.TextField(label="Щільність феромагнетика, г/см³", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    q.value = 7.35
+    J = ft.TextField(label="Густина струму, А/мм²", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    J.value = 5.09295817894066
+    d2w = ft.TextField(label="Діаметр дроту, мм", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    d2w.value = 0.111803398874989
+    Nw = ft.TextField(label="Кількість жил дроту", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    Nw.value = 10
+    Kw = ft.TextField(label="Коефіцієнт заповнення дротом", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    Kw.value = 0.6
+    mCost = ft.TextField(label="Ціна осердя за 1 кг, грн", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    mCost.value = 4000
+    wCost = ft.TextField(label="Ціна дроту за 1 кг, грн", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    wCost.value = 500
+    dmin = ft.TextField(label="Мін. внутрішній діаметр, мм", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    dmin.value = 0
+    dmax = ft.TextField(label="Макс. зовнішній діаметр, мм", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    dmax.value = 1000
+    Nmax = ft.TextField(label="Макс. можлива к-сть витків дроту", height=40, width=250, content_padding=ft.padding.all(5), text_align='center')
+    Nmax.value = 10_000
+
+    #panels
+    mParams = ft.Container(content=ft.Column([ft.Text('Параметри осердя', size=18), Bmax, Kf, q], horizontal_alignment=ft.CrossAxisAlignment.CENTER, spacing=20), bgcolor=ft.colors.TEAL_50, padding=20, border_radius=10)
+    mParams.alignment = ft.alignment.center
+
+    wParams = ft.Container(content=ft.Column([ft.Text('Параметри дроту', size=18), J, d2w, Nw, Kw], horizontal_alignment=ft.CrossAxisAlignment.CENTER, spacing=20), bgcolor=ft.colors.ORANGE_50, padding=20, border_radius=10)
+    wParams.alignment = ft.alignment.center
+
+    costParams = ft.Container(content=ft.Column([ft.Text('Вартість матеріалів', size=18), mCost, wCost], horizontal_alignment=ft.CrossAxisAlignment.CENTER, spacing=20), bgcolor=ft.colors.BROWN_50, padding=20, border_radius=10)
+    costParams.alignment = ft.alignment.center
+
+    limitParams = ft.Container(content=ft.Column([ft.Text('Обмеженя параметрів', size=18), dmin, dmax, Nmax], horizontal_alignment=ft.CrossAxisAlignment.CENTER, spacing=20), bgcolor=ft.colors.BLUE_50, padding=20, border_radius=10)
+    limitParams.alignment = ft.alignment.center
+
+
+
 
 
     #GET DATA_FRAME
@@ -28,14 +68,20 @@ async def main(page: ft.Page):
     Mu_list = [19, 26, 40, 60, 90, 125, 147, 160, 175, 200, 300, 400, 500, 700, 1000, 3000, 10000, 30000, 50000, 90000]
     
     
-
-    N_df = await N_culculate(1, 50, H_list, Mu_list)         #<<<<<main calculation
+    params_ = [Bmax, Kf, q, J, d2w, Nw, Kw, dmin, dmax, Nmax]
+    params = [field.value for field in params_]
+    N_df = await N_culculate(I.value, L.value, H_list, Mu_list, params)         #<<<<<main calculation
 
 
 
     async def N_culc(e):
-        N_df = await N_culculate(float(I.value), float(L.value), H_list, Mu_list) #>>>>main calculation
+        params = [float(field.value) for field in params_]
+        N_df = await N_culculate(float(I.value), float(L.value), H_list, Mu_list, params) #>>>>main calculation
         update_btns(btns_matrix, N_df)
+
+        print(I.value, L.value)
+        print(params)
+        print(Nmax.value)
         await page.update_async()
 
 
@@ -53,7 +99,7 @@ async def main(page: ft.Page):
             btn_index = int(e.control.data[-2:])-1
             btns_matrix[key][btn_index].style = cell_style(0)
 
-        output_text.value = f"THE COST IS {e.control.text}"
+        output_text.value = f"ЦIНА ДРОСЕЛЯ - {e.control.text}"
         await page.update_async()
 
 
@@ -90,8 +136,6 @@ async def main(page: ft.Page):
     async def upd_H(e):
         H_list[H_index] = int(new_H.value)
         H_btns[H_index].text = new_H.value
-        #N_df = await N_culculate(float(I.value), float(L.value), H_list, Mu_list) #>>>>main calculation
-        #update_btns(btns_matrix, N_df)
         popup_H.visible = False
         await page.update_async()
 
@@ -103,7 +147,6 @@ async def main(page: ft.Page):
         i = H_list.index(height)     
         Value = ft.ElevatedButton(f"{height}", style=cell_style(-2), data=i, on_click=upd_H_Panel)
         H_btns.append(Value)
-
 
     Mu_btns = []
     for Mu in Mu_list:
@@ -136,16 +179,20 @@ async def main(page: ft.Page):
     
 
 
+
+
+
+    #PAGE_BLOCKS - DESIGN -----------------------------------------------------------------
     async def to_Settings(route):
         await page.go_async("/settings")
-    #PAGE_BLOCKS - DESIGN
-    title_text = ft.AppBar(title=ft.Text('CHOKE COST CALCULATOR', color=ft.colors.CYAN_900, size=22),
+
+    title_text = ft.AppBar(title=ft.Text('КАЛЬКУЛЯТОР ВАРТОСТI ДРОСЕЛЯ', color=ft.colors.CYAN_900, size=22),
                         bgcolor=ft.colors.TEAL_50,
                         center_title=True,
                         actions=[ft.IconButton(ft.icons.SETTINGS, on_click=to_Settings)])
 
       
-    culc_btn = ft.Container(ft.ElevatedButton('CALCULATE', on_click=N_culc))
+    culc_btn = ft.Container(ft.ElevatedButton('РОЗРАХУВАТИ', on_click=N_culc))
     culc_btn.padding = ft.padding.only(bottom=20)
 
     output_text = ft.Text('Please select N-cell from table', color=ft.colors.BLUE_900, size=16) 
@@ -176,17 +223,18 @@ async def main(page: ft.Page):
                 output_text,
                 matrixCtr],
                 horizontal_alignment='center',
-                vertical_alignment = ft.MainAxisAlignment.START,
                 scroll=True))
         if page.route == "/settings":
             page.views.append(
                 ft.View("/settings",
-                    [ft.AppBar(title=ft.Text("Additional Settings"), bgcolor=ft.colors.DEEP_ORANGE_100, center_title=True),
-                    ft.TextField(label='Values1'),
-                    ft.TextField(label='Values2'),
-                    ft.TextField(label='Values3'),
-                    ft.TextField(label='Values4'),
-                    ]))
+                    [ft.AppBar(title=ft.Text("Додатковi Параметры", color=ft.colors.BROWN_900, size=22), bgcolor=ft.colors.DEEP_ORANGE_100, center_title=True),
+                    mParams,
+                    wParams,
+                    costParams,
+                    limitParams],
+                    horizontal_alignment='center',
+                    scroll=True
+                    ))
         await page.update_async()
 
     async def back(route):
@@ -199,16 +247,8 @@ async def main(page: ft.Page):
     page.on_view_pop = back
 
     await page.go_async(page.route)
-    # await page.add_async(title_text,
-    #         ft.Container(content=ft.Row([I, L], alignment=ft.MainAxisAlignment.CENTER, spacing=30, scroll='hidden', wrap=True), padding=20),
-    #         culc_btn,
-    #         popup_H,
-    #         popup_Mu,
-    #         output_text,
-    #         matrixCtr)
-    
-    # await page.update_async()
 
+ 
 
-ft.app(target=main, view=ft.WEB_BROWSER, port=8000)
+ft.app(target=main, view=ft.WEB_BROWSER, port=8000, route_url_strategy='hash')
 
